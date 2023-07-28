d25e27c66e7e08b4f2eb2f80a4ee22a679d6bc7a
BastianDeux <72988590+BastianDeux@users.noreply.github.com>
2023-07-28 16:59:48 -0500

    adapt to havoc

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_rhode.mk
+    $(LOCAL_DIR)/havoc_rhode.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_rhode-user \
-    lineage_rhode-userdebug \
-    lineage_rhode-eng
+    havoc_rhode-user \
+    havoc_rhode-userdebug \
+    havoc_rhode-eng
